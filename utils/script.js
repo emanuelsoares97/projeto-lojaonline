87eb6f6b54678ee77ee0fdca425843d6793bda81
emanuelsoares97 <tutasoares17@gmail.com>
2025-02-26 19:09:19 +0000

    alot of things

--- utils/script.js
+++ utils/script.js
@@ -56,5 +56,20 @@ document.getElementById('produtos').addEventListener('click', function(e) {
 });
 
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.querySelectorAll('.slider img');
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
+}
+
+
 
 
