97afc336a1646016a8415af2fccfd8313269e0a0
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-24 19:29:56 +0100

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -2,7 +2,7 @@
 const API_CONFIG = {
     // URL base da API - altera baseado no ambiente
     baseURL: window.location.hostname.includes('github.io') 
-        ? 'https://projeto-lojaonline-backend.onrender.com' // URL do Render em produção
+        ? 'https://projeto-lovepulseiras-api.onrender.com' // URL do Render em produção
         : 'http://localhost:5000' // URL local para desenvolvimento
 };
 
