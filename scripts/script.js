d7859da3979716fe98c597daa62eb414bd2c142f
emanuelsoares97 <tutasoares17@gmail.com>
2025-05-24 17:36:29 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
-
-//função para fechar e abrir menu
+// Gestão do menu de navegação
 document.addEventListener("DOMContentLoaded", function () {
     const menu = document.querySelector(".menu");
     const menuIcon = document.querySelector(".menu-icon");
 
     menuIcon.addEventListener("click", function () {
-        menu.classList.toggle("active"); // ao clicar aparece e desaparece
+        menu.classList.toggle("active"); // Alternância da visibilidade do menu
     });
 });
 
